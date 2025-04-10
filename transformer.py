d0c0100bc494593da45a6f60c99d0a36eac6b866
Ben Limpanukorn <ben@limpanu.com>
2025-04-09 18:13:23 -0700

    Fix serializer and transformer
    - Delete the children of Identifier nodes (children represent lexer
    fragments) when replacing the src
    - Don't add spaces for certain lexer rules

--- transformer.py
+++ transformer.py
@@ -1,19 +1,28 @@
 def anonymizer_transformer(root):
+    def delete_children(node):
+        for child in node.children:
+            child.parent = None
+        node.children = []
+        assert node.children == []
+
     def _walk(node):
         nonlocal idx
 
-        if node.name == 'table_ref':
+        if node.name == "table_ref":
             for child in node.children:
-                if child.name=="Identifier":
-                    child.src="table_name"
-        elif node.name=="columnref":
+                if child.name == "Identifier":
+                    child.src = "table_name"
+                    delete_children(child)
+        elif node.name == "columnref":
             for child in node.children:
-                if child.name=="Identifier":
-                    child.src="column_name"
-        elif node.name=="collabel":
+                if child.name == "Identifier":
+                    child.src = "column_name"
+                    delete_children(child)
+        elif node.name == "collabel":
             for child in node.children:
-                if child.name=="Identifier":
-                    child.src="column_label"
+                if child.name == "Identifier":
+                    child.src = "column_label"
+                    delete_children(child)
 
         for child in node.children:
             _walk(child)
@@ -21,4 +30,3 @@ def anonymizer_transformer(root):
     idx = 0
     _walk(root)
     return root
-
